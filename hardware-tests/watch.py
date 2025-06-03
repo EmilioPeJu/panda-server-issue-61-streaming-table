f92b99982b524ef6b0c85f47bbaaea3bf101bfa9
Emilio Perez <emiliopeju@gmail.com>
2025-06-03 14:40:07 +0100

    Add script to watch block fields

---
+++ hardware-tests/watch.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import argparse
+import time
+
+from tui import TuiManager
+from panda import PandaClient
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('host')
+    parser.add_argument('name')
+    parser.add_argument('--watch-period', type=float, default=1.0)
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    client = PandaClient(args.host)
+    client.connect()
+    fields = []
+    for field_name in args.name.split(','):
+        fields.extend(
+            [client[i] for i in client.get_field_names_with(field_name)])
+
+    tui = TuiManager()
+    def draw():
+        tui.clear()
+        tui.reset_line()
+        for field in fields:
+            tui.add_str(f'{field.path}: {field.get()}')
+
+    tui.add_draw_callback(draw)
+    while True:
+        draw()
+        tui.process_events()
+        try:
+            time.sleep(args.watch_period)
+        except KeyboardInterrupt:
+            break
+
+    tui.quit()
+    client.close()
+
+if __name__ == '__main__':
+    main()
